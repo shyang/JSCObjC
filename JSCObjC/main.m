3031cac30b0e8c313c91305857cf449966db3a97
syang <syang@local>
2016-06-17 22:44:40 +0800

    Initial Commit

---
+++ JSCObjC/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  JSCObjC
+//
+//  Created by shaohua on 6/17/16.
+//  Copyright © 2016 syang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    }
+}
